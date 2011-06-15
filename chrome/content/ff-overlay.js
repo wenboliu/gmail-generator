4cfb342fa8bc192892b7bdf45ea7266c6c17dd54
Wenbo_Liu <wbliu@thoughtworks.com>
2011-06-16 02:19:39 +0800

    finished the first version

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -1,11 +1,42 @@
 gmailgenerator.onFirefoxLoad = function(event) {
   document.getElementById("contentAreaContextMenu")
-          .addEventListener("popupshowing", function (e){ gmailgenerator.showFirefoxContextMenu(e); }, false);
+          .addEventListener("popupshowing", function (e){gmailgenerator.showFirefoxContextMenu(e);}, false);
 };
 
 gmailgenerator.showFirefoxContextMenu = function(event) {
   // show or hide the menuitem based on what the context menu is on
-  document.getElementById("context-gmailgenerator").hidden = gContextMenu.onImage;
+  document.getElementById("context-gmailgenerator").hidden = gmailgenerator.isHiddenContextMenu(event);
 };
 
-window.addEventListener("load", function () { gmailgenerator.onFirefoxLoad(); }, false);
+
+gmailgenerator.isHiddenContextMenu = function(event) {
+  var strDocumentUrl = document.commandDispatcher.focusedWindow.document.URL;
+  if (strDocumentUrl.indexOf("mail.google.com") == -1) {
+      return true;
+  }
+  
+  var canvasFrame = window.content.document.getElementById("canvas_frame");
+  if (!canvasFrame) {
+      return true;
+  }
+  var canvasDoc = canvasFrame.contentDocument;
+  var toElems = canvasDoc.getElementsByName("to");
+  if (!toElems || toElems.length < 1) {
+      return true;
+  }
+  this.toEle = toElems[0];
+  var subElems = canvasDoc.getElementsByName("subject");
+  if (!subElems || subElems.length < 1) {
+      return true;
+  }
+  this.subjectEle = subElems[0];
+  var bodyElems = canvasDoc.getElementsByName("body");
+  if (!bodyElems || bodyElems.length < 1) {
+      return true;
+  }
+  this.bodyEle = bodyElems[0];
+  return false;
+};
+
+
+window.addEventListener("load", function () {gmailgenerator.onFirefoxLoad();}, false);
--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -2,52 +2,16 @@ var gmailgenerator = {
   onLoad: function() {
     // initialization code
     this.initialized = true;
-    this.displayUrl = "www.google.com";
-    this.displayMenu = false;
+    this.toEle = null;
+    this.subjectEle = null;
+    this.bodyEle = null;
     this.strings = document.getElementById("gmailgenerator-strings");
-  },
-  onGeneratorContextMenu: function(event) 
-  {
-       var strDocumentUrl = document.commandDispatcher.focusedWindow.document.URL;
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                strDocumentUrl);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                "ddd" + this.displayMenu);
-                                
-        var strDocumentUrl = document.commandDispatcher.focusedWindow.document.URL;
-	
-        this.displayMenu = false;
-        
-        promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.displayMenu);
-        
-        if(event.target.tagName == "TEXTAREA" ||
-           event.target.tagName == "textarea" ||
-           event.target.tagName == "input")
-	{
-            if (event.target.name == "to" || 
-                event.target.name == "cc" ||
-                event.target.name == "body" ||
-                event.target.name == "subject")  
-            {
-                this.displayMenu = strDocumentUrl.indexOf(this.displayUrl) != -1;
-            }
-		
-	}
-        document.getElementById("context-gmailgenerator-up").setAttribute("hidden", this.displayMenu);
-        document.getElementById("context-gmailgenerator").setAttribute("hidden", this.displayMenu);
-         promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.displayMenu);
   },
 
   onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    var strDocumentUrl = document.commandDispatcher.focusedWindow.document.URL;
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                strDocumentUrl);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                "ddd" + this.displayMenu);
+    this.toEle.value = "ToEmailAddress";
+    this.subjectEle.value = "subject";
+    this.bodyEle.value = "test body content";
   },
 
   onToolbarButtonCommand: function(e) {
